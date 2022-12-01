c85ffa6dbd8913cb9a83afe4104ee9f07988963b
Alejandro <aescamilla.2025@carmelhs.org>
2022-11-30 22:25:17 -0600

    Distance calculation, only one cone at a time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/RedPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/RedPipeline.java
@@ -14,6 +14,7 @@ import static org.opencv.imgproc.Imgproc.moments;
 import static org.opencv.imgproc.Imgproc.putText;
 import static org.opencv.imgproc.Imgproc.rectangle;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
@@ -21,17 +22,29 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RedPipeline extends OpenCvPipeline {
 
-    public Scalar LOWER_BOUND_COLOR = new Scalar(0, 201, 181);
+    public Scalar LOWER_BOUND_COLOR = new Scalar(0, 173, 181);
     public Scalar HIGHER_BOUND_COLOR = new Scalar(187, 255, 255);
+
+    Telemetry telemetry;
+
+    public RedPipeline(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    double closestConeDist = 50.0;
+    int numOfContours = 0;
 
     @Override
     public Mat processFrame(Mat input) {
 
+        int oldNumOfContours = numOfContours;
 
         Mat hsv_image = new Mat();
         cvtColor(input, hsv_image, COLOR_RGB2HSV);
@@ -50,15 +63,27 @@ public class RedPipeline extends OpenCvPipeline {
 
         findContours(mask, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-        int closestConeH = 0;
-
-        if (!contours.isEmpty()){
-            closestConeH = boundingRect(contours.get(0)).height;
-        }
-        else{
+        if (contours.isEmpty()){
             System.out.println("No contours found");
+            closestConeDist = 50.0;
+            return input;
         }
 
+        // find all of the contours greater than 100px, and add them to a list
+        List<MatOfPoint> filteredContours = new ArrayList<>();
+        for (MatOfPoint contour : contours) {
+            if (boundingRect(contour).width > 100) {
+                filteredContours.add(contour);
+            }
+        }
+        numOfContours = filteredContours.size();
+
+        if (numOfContours != oldNumOfContours) {
+            closestConeDist = 50.0;
+        }
+
+
+        telemetry.addData("Number of contours", filteredContours.size());
         for (MatOfPoint contour : contours){
             int x, y, w, h;
             x = boundingRect(contour).x;
@@ -66,30 +91,36 @@ public class RedPipeline extends OpenCvPipeline {
             w = boundingRect(contour).width;
             h = boundingRect(contour).height;
 
-            if (h > 60){
-                if (h > closestConeH){
-                    closestConeH = h;
+            double focalLength = ((421 * (12 / 39.7)) / (11 / 39.7));
+            double distanceIN = (12 * focalLength) / w - 1;
+            double distanceM = distanceIN / 39.7;
+            BigDecimal dist = new BigDecimal(distanceIN).setScale(2, RoundingMode.HALF_UP);
+            if (w > 100){
+                if (dist.doubleValue() <= closestConeDist){
+                    closestConeDist = dist.doubleValue();
+                    telemetry.addData("Closet Cone Distance", dist.doubleValue());
                     rectangle(input, new Point(x, y), new Point(x + w, y + h), new Scalar(0, 255, 0), 2);
                     Moments M = moments(contour);
                     int cX = (int) (M.m10 / M.m00);
                     int cY = (int) (M.m01 / M.m00);
 
-                    circle(input, new Point(cX, cY), 7, new Scalar(255, 0, 0), -1);
+                    circle(input, new Point(cX, cY), 7, new Scalar(0, 0, 255), -1);
 
-                    putText(input, h + "px", new Point(cX - 20, cY + 25), FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(255, 255, 255), 2);
+                    putText(input, "Distance: " + dist + "in", new Point(cX - 100, cY + 50), FONT_HERSHEY_SIMPLEX, 0.75, new Scalar(255, 255, 255), 2);
 
                     if (cX > 300 && cX < 340) {
                         circle(input, new Point(cX, cY), 7, new Scalar(0, 255, 0), -1);
-                        System.out.println("Centered");
+                        telemetry.addLine("Centered");
                     }
                     else {
-                        System.out.println("Not Centered: " + cX);
+                        telemetry.addData("Not Centered: ", cX);
                     }
                 }
                 else {
                     rectangle(input, new Point(x, y), new Point(x + w, y + h), new Scalar(255, 0, 0), 3);
                 }
             }
+
         }
 
         //release all Mats
@@ -98,6 +129,7 @@ public class RedPipeline extends OpenCvPipeline {
         maskImg.release();
         hierarchy.release();
 
+        telemetry.update();
         return input;
     }
 
